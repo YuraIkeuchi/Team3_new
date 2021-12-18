c914ec6f0234c876959f32fab2b8a694aaa1142e
IkeuchiYura <k020g1233@g.neec.ac.jp>
2021-12-18 11:23:59 +0900

    シーン管理(簡素)

--- main.cpp
+++ main.cpp
@@ -22,6 +22,7 @@
 #include "DirectXTex/d3dx12.h"
 
 #include "imgui\imgui.h"
+#include <cassert>
 
 #pragma comment(lib,"d3dcompiler.lib")
 #pragma comment(lib,"d3d12.lib")
@@ -41,23 +42,6 @@ LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 	return DefWindowProc(hwnd, msg, wparam, lparam);
 }
 
-bool KingBoxCollision(XMFLOAT3 object1, XMFLOAT3 radius1, XMFLOAT3 object2, XMFLOAT3 radius2) {
-	float disX1 = object1.x + radius1.x;
-	float disX2 = object2.x - radius2.x;
-	float disX3 = object1.x - radius1.x;
-	float disX4 = object2.x + radius2.x;
-	float disY1 = object1.y + radius1.y;
-	float disY2 = object2.y - radius2.y;
-	float disY3 = object1.y - radius1.y;
-	float disY4 = object2.y + radius2.y;
-	float disZ1 = object1.z + radius1.z;
-	float disZ2 = object2.z - radius2.z;
-	float disZ3 = object1.z - radius1.z;
-	float disZ4 = object2.z + radius2.z;
-
-	//xとyはそれぞれ中心座標として計算する
-	return disX1 > disX2 && disX4 > disX3 && disY1 > disY2 && disY4 > disY3 && disZ1 > disZ2 && disZ4 > disZ3;
-}
 //テクスチャの最大枚数
 const int SpriteSRVCount = 512;
 
@@ -157,12 +141,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	// スプライト共通テクスチャ読み込み
 	/*SpriteCommonLoadTexture(spriteCommon, 0, L"Resources/texture.png", dxCommon->GetDev());
 	SpriteCommonLoadTexture(spriteCommon, 1, L"Resources/house.png", dxCommon->GetDev());*/
-	Sprite::LoadTexture(0, L"Resources/texture.png");
-	Sprite::LoadTexture(1, L"Resources/house.png");
-	sprite[0] = Sprite::Create(0, { 50.0f,50.0f });
-	sprite[1] = Sprite::Create(1, { 500.0f,500.0f });
-	sprite[0]->SetSize({ 150.5f,150.5f });
-	sprite[1]->SetSize({ 150.5f,150.5f });
+	Sprite::LoadTexture(0, L"Resources/Title.png");
+	Sprite::LoadTexture(1, L"Resources/END.png");
+	sprite[0] = Sprite::Create(0, { 0.0f,0.0f });
+	sprite[1] = Sprite::Create(1, { 0.0f,0.0f });
 	//オーディオ
 	const int AudioMax = 3;
 	Audio* audio = new Audio;
@@ -237,7 +219,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	PlayerPosition = player->GetPosition();
 
 	player->SetPosition(PlayerPosition);
-	XMFLOAT3 OldPlayerPosition;
 	XMFLOAT2 SpritePosition = sprite[0]->GetPosition();
 	int HitFlag = 0;
 	int RightHitFlag = 0;
@@ -245,6 +226,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	int UpHitFlag = 0;
 	int DownHitFlag = 0;
 	float JumpG = 0.0f;
+	int Scene = 0;
+	enum Scene {
+		title,
+		gamePlay,
+		gameOver,
+		gameClear
+	};
 	while (true) {
 		//ウィンドウメッセージ処理
 		if (winApp->ProcessMessage()) {
@@ -255,92 +243,97 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		}
 		PlayerPosition = player->GetPosition();
 		SpritePosition = sprite[0]->GetPosition();
+		//毎フレーム処理
 
-		//障害物との当たり判定
-
-			//キーの更新
+		//キーの更新
 		input->Update();
-		//グラフィックのやつ各とこ
-		//まいフレーム処理
-		//ジャンプ処理
-		OldPlayerPosition.x = PlayerPosition.x;
-		OldPlayerPosition.y = PlayerPosition.y;
-		if (input->TriggerKey(DIK_SPACE)) {
-			JumpG = -0.8f;
-		}
 
-		if (input->PushKey(DIK_UP)) {
-			PlayerPosition.y += 0.5f;
-		}
-
-		if (input->PushKey(DIK_DOWN) && DownHitFlag == 0) {
-			PlayerPosition.y -= 0.5f;
-		}
-
-		if (input->PushKey(DIK_LEFT) && LeftHitFlag == 0) {
-			PlayerPosition.x -= 0.5f;
-		}
-		if (input->PushKey(DIK_RIGHT) && RightHitFlag == 0) {
-			PlayerPosition.x += 0.5f;
-		}
-		if (input->PushKey(DIK_D) || input->PushKey(DIK_A)) {
-			if (input->PushKey(DIK_D)) {
-				angle += XMConvertToRadians(1.0f);
-			} else if (input->PushKey(DIK_A)) {
-				angle -= XMConvertToRadians(1.0f);
-			}
-			eye.x = -100 * sinf(angle);
-			eye.z = -100 * cosf(angle);
-			matview = XMMatrixLookAtLH(XMLoadFloat3(&eye), XMLoadFloat3(&target), XMLoadFloat3(&up));
-		}
-
-		PlayerPosition.y -= JumpG;
-		JumpG += 0.025f;
-
-		if (PlayerPosition.y <= -10.0f) {
-			JumpG = 0.0f;
-			PlayerPosition.y = -10.0f;
-		}
-
-		for (int i = 0; i < OBJECT_NUM; i++) {
-			//上
-			if ((ObjectPosition[i].y - PlayerPosition.y <= 5.2) && (ObjectPosition[i].y - PlayerPosition.y >= 1.0)
-				&& (ObjectPosition[i].x - PlayerPosition.x <= 4.8) && (ObjectPosition[i].x - PlayerPosition.x >= -4.8)) {
-				JumpG = JumpG * -1;
+		if (Scene == title) {
+			if (input->TriggerKey(DIK_SPACE)) {
+				Scene = gamePlay;
 			}
 
-			//下
-			if ((ObjectPosition[i].y - PlayerPosition.y >= -5.2) && (ObjectPosition[i].y - PlayerPosition.y <= -1.0)
-				&& (ObjectPosition[i].x - PlayerPosition.x <= 4.8) && (ObjectPosition[i].x - PlayerPosition.x >= -4.8)) {
+		}
+		
+		if (Scene == gamePlay) {
+			if (input->PushKey(DIK_UP)) {
+				PlayerPosition.y += 0.5f;
+			}
+
+			if (input->PushKey(DIK_DOWN) && DownHitFlag == 0) {
+				PlayerPosition.y -= 0.5f;
+			}
+
+			if (input->PushKey(DIK_LEFT) && LeftHitFlag == 0) {
+				PlayerPosition.x -= 0.5f;
+			}
+			if (input->PushKey(DIK_RIGHT) && RightHitFlag == 0) {
+				PlayerPosition.x += 0.5f;
+			}
+
+			//ジャンプ処理
+			if (input->TriggerKey(DIK_SPACE)) {
+				JumpG = -0.8f;
+			}
+
+
+			PlayerPosition.y -= JumpG;
+			JumpG += 0.025f;
+
+			if (PlayerPosition.y <= -10.0f) {
 				JumpG = 0.0f;
-				DownHitFlag = 1;
-				if (PlayerPosition.y <= ObjectPosition[i].y / 2) {
-					PlayerPosition.y = ObjectPosition[i].y / 2;
+				PlayerPosition.y = -10.0f;
+			}
+
+			//障害物との当たり判定
+			for (int i = 0; i < OBJECT_NUM; i++) {
+				//上
+				if ((ObjectPosition[i].y - PlayerPosition.y <= 5.2) && (ObjectPosition[i].y - PlayerPosition.y >= 1.0)
+					&& (ObjectPosition[i].x - PlayerPosition.x <= 4.8) && (ObjectPosition[i].x - PlayerPosition.x >= -4.8)) {
+					JumpG = JumpG * -1;
 				}
-			} else {
-				DownHitFlag = 0;
+
+				//下
+				if ((ObjectPosition[i].y - PlayerPosition.y >= -5.2) && (ObjectPosition[i].y - PlayerPosition.y <= -1.0)
+					&& (ObjectPosition[i].x - PlayerPosition.x <= 4.8) && (ObjectPosition[i].x - PlayerPosition.x >= -4.8)) {
+					JumpG = 0.0f;
+					DownHitFlag = 1;
+					if (PlayerPosition.y <= ObjectPosition[i].y / 2) {
+						PlayerPosition.y = ObjectPosition[i].y / 2;
+					}
+				} else {
+					DownHitFlag = 0;
+				}
+
+				//右
+				if ((ObjectPosition[i].x - PlayerPosition.x <= 5) && (ObjectPosition[i].x - PlayerPosition.x >= 4)
+					&& (ObjectPosition[i].y - PlayerPosition.y <= 5) && (ObjectPosition[i].y - PlayerPosition.y >= -5)) {
+					RightHitFlag = 1;
+					break;
+				} else {
+					RightHitFlag = 0;
+				}
+
+				//左
+				if ((ObjectPosition[i].x - PlayerPosition.x >= -5) && (ObjectPosition[i].x - PlayerPosition.x <= -4) &&
+					(ObjectPosition[i].y - PlayerPosition.y <= 5) && (ObjectPosition[i].y - PlayerPosition.y >= -5)) {
+					LeftHitFlag = 1;
+					break;
+				} else {
+					LeftHitFlag = 0;
+				}
 			}
 
-			//右
-			if ((ObjectPosition[i].x - PlayerPosition.x <= 5) && (ObjectPosition[i].x - PlayerPosition.x >= 4)
-				&& (ObjectPosition[i].y - PlayerPosition.y <= 5) && (ObjectPosition[i].y - PlayerPosition.y >= -5)) {
-				RightHitFlag = 1;
-				break;
-			} else {
-				RightHitFlag = 0;
-			}
-
-			//左
-			if ((ObjectPosition[i].x - PlayerPosition.x >= -5) && (ObjectPosition[i].x - PlayerPosition.x <= -4) &&
-				(ObjectPosition[i].y - PlayerPosition.y <= 5) && (ObjectPosition[i].y - PlayerPosition.y >= -5)) {
-				LeftHitFlag = 1;
-				break;
-			} else {
-				LeftHitFlag = 0;
+			if (input->TriggerKey(DIK_R)) {
+				Scene = gameClear;
 			}
 		}
 
-	
+		if (Scene == gameClear) {
+			if (input->TriggerKey(DIK_S)) {
+				Scene = title;
+			}
+		}
 		for (int i = 0; i < _countof(object); i++) {
 			object[i]->Update();
 			//ルートシグネチャの設定コマンド
@@ -351,8 +344,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		player->SetPosition(PlayerPosition);
 
 		//びょうがこまんど
-
-
 		//x,y座標のデバッグログ
 		wchar_t str[256];
 
@@ -364,42 +355,38 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		/*swprintf_s(str, L"ObjectPosition[1].y - ObjectPosition[0].y:%f\n", ObjectPosition[1].y - ObjectPosition[0].y);
 		OutputDebugString(str);*/
 		//描画コマンド
-
-		Object::PreDraw(dxCommon->GetCmdList());
 		dxCommon->PreDraw();
-		ImGui::Begin("test");
-		if (ImGui::TreeNode("Debug"))
-		{
-			if (ImGui::TreeNode("Player"))
-			{
-				ImGui::Indent();
-				ImGui::SliderFloat("Position.x", &PlayerPosition.x, 50, -50);
-				ImGui::SliderFloat("Position.y", &PlayerPosition.y, 50, -50);
-				ImGui::Unindent();
-				ImGui::TreePop();
-			}
-			ImGui::TreePop();
-		}
-		ImGui::Indent();
-		ImGui::Unindent();
-		ImGui::End();
-		//プリティ部形状の設定コマンド
+		////4.描画コマンドここから
 		dxCommon->GetCmdList()->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-		for (int i = 0; i < _countof(object); i++) {
-			object[i]->Draw();
-		}
-
-		player->Draw();
+		//4.描画コマンドここまで
+#pragma endregion
+#pragma region//描画
+		//背景スプライト描画前処理
 		Sprite::PreDraw(dxCommon->GetCmdList());
-
-		sprite[0]->Draw();
-
-		//sprite[1]->Draw();
+		if (Scene == title) {
+			sprite[0]->Draw();
+		}
 		Sprite::PostDraw();
-		////描画コマンド　ここまで
-		dxCommon->PostDraw();
+		Object::PreDraw(dxCommon->GetCmdList());
+		//背景
+		if (Scene == gamePlay) {
+			player->Draw();
+			for (int i = 0; i < _countof(object); i++) {
+				object[i]->Draw();
+			}
+		}
+		Sprite::PreDraw(dxCommon->GetCmdList());
+		if (Scene == gameClear) {
+			sprite[1]->Draw();
+		}
+	
+		Sprite::PostDraw();
+		// 3Dオブジェクト描画後処理
 		Object::PostDraw();
-
+	
+#pragma endregion
+#pragma region//ゲーム外処理２
+		dxCommon->PostDraw();
 	}
 	winApp->Finalize();
 	for (int i = 0; i < SpriteMax; i++) {
